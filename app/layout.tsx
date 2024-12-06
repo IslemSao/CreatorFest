78fb8f91d651a28577209dc9f4c4a36296c65e53
AIT OUHAMOU SAMY ISLEM <ms_aitouhamou@esi.dz>
2024-12-06 22:46:13 +0100

    V1.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Conference Website',
+  title: 'Creator Fest',
   description: 'A collaborative conference website project',
 };
 
